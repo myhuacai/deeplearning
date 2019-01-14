56f98a4685fb5a3195963682b9041874e197a7fd
myhuacai <416686950@qq.com>
2019-01-14 16:47:10 +0800

    自然语言处理9.2，神经语言模型

--- TFdemo/7.2.1.py
+++ TFdemo/7.2.1.py
@@ -4,18 +4,44 @@
 import  matplotlib.pyplot as plt
 import tensorflow as tf
 
+# 读取图片
+image_data = tf.gfile.FastGFile("data/dog3.jpg", 'br').read()
+#
+with tf.Session() as sess:
+    img_data = tf.image.decode_jpeg(image_data)
+    plt.imshow(img_data.eval())
+    plt.show()
 
+    # 将图片数据转换成实数类型
+    img_data = tf.image.convert_image_dtype(img_data, dtype=tf.float32)
+    # # 图像上下翻转
+    # fliped1 = tf.image.flip_up_down(img_data)
+    # plt.imshow(fliped1.eval())
+    # 左右翻转
+    fliped = tf.image.flip_left_right(img_data)
+    plt.imshow(fliped.eval())
+    # 沿着对角线翻转
+    # transposed = tf.image.transpose_image(img_data)
+    # plt.imshow(transposed.eval())
+    # # 以50%概率上下翻转
+    # fliped = tf.image.random_flip_up_down(img_data)
+    # plt.imshow(fliped.eval())
+    # # 以50%概率左右翻转
+    # fliped = tf.image.random_flip_left_right(img_data)
+    # plt.imshow(fliped.eval())
+    plt.show()
 # 1.图像编码处理
-image_data = tf.gfile.FastGFile("data/5.jpg",'br').read()
+# 读取图片
+# image_data = tf.gfile.FastGFile("data/dog1.jpg",'br').read()
 
-# print(image_data)
+# # # print(image_data)
 # with tf.Session() as sess:
+#     # 解码原图片并展示出来
 #     img_data = tf.image.decode_jpeg(image_data)
-#     # print(img_data.eval())
 #     plt.imshow(img_data.eval())
 #     plt.show()
 #     encoded_image = tf.image.encode_jpeg(img_data)
-#     print(sess.run(encoded_image))
+#     # print(sess.run(encoded_image))
 #     with tf.gfile.GFile("data/output","wb") as f:
 #         f.write(encoded_image.eval())
 
@@ -32,11 +58,13 @@ image_data = tf.gfile.FastGFile("data/5.jpg",'br').read()
 
 # with tf.Session() as sess:
 #     img_data = tf.image.decode_jpeg(image_data)
+#     # 将图片数据转换成实数类型
 #     img_data = tf.image.convert_image_dtype(img_data, dtype=tf.float32)
-#     # print(img_data.eval())
+#     # 重新转换成300X900的图片，并展示出调整后的图片
 #     resized = tf.image.resize_images(img_data,[300,900],method=1)
 #     plt.imshow(resized.eval())
 #     plt.show()
+
 #1920X1200
 # with tf.Session() as sess:
 #     img_data = tf.image.decode_jpeg(image_data)
@@ -53,19 +81,19 @@ image_data = tf.gfile.FastGFile("data/5.jpg",'br').read()
 
 # 3.图像翻转
 # with tf.Session() as sess:
-#     img_data = tf.image.decode_jpeg(image_data)
-#     img_data = tf.image.convert_image_dtype(img_data, dtype=tf.float32)
-#     # fliped1 = tf.image.flip_up_down(img_data)
-#     # plt.imshow(fliped1.eval())
-#     # fliped = tf.image.flip_left_right(img_data)
-#     # plt.imshow(fliped.eval())
-#     # transposed = tf.image.transpose_image(img_data)
-#     # plt.imshow(transposed.eval())
-#     # fliped = tf.image.random_flip_up_down(img_data)
-#     # plt.imshow(fliped.eval())
-#     fliped = tf.image.random_flip_left_right(img_data)
-#     plt.imshow(fliped.eval())
-#     plt.show()
+    # img_data = tf.image.decode_jpeg(image_data)
+    # img_data = tf.image.convert_image_dtype(img_data, dtype=tf.float32)
+    # fliped1 = tf.image.flip_up_down(img_data)
+    # plt.imshow(fliped1.eval())
+    # fliped = tf.image.flip_left_right(img_data)
+    # plt.imshow(fliped.eval())
+    # transposed = tf.image.transpose_image(img_data)
+    # plt.imshow(transposed.eval())
+    # fliped = tf.image.random_flip_up_down(img_data)
+    # plt.imshow(fliped.eval())
+    # fliped = tf.image.random_flip_left_right(img_data)
+    # plt.imshow(fliped.eval())
+    # plt.show()
 
 # 4.图像色彩调整
 # with tf.Session() as sess:
@@ -97,23 +125,23 @@ image_data = tf.gfile.FastGFile("data/5.jpg",'br').read()
 #     plt.show()
 
 # 5.处理标注窗
-
-with tf.Session() as sess:
-    img_data = tf.image.decode_jpeg(image_data)
-    img_data = tf.image.convert_image_dtype(img_data, dtype=tf.float32)
-    # 将图片缩小一些，这样可视化能让标注框更加清楚
-    img_data = tf.image.resize_images(img_data,[180,267],method=1)
-    batched = tf.expand_dims(tf.image.convert_image_dtype(img_data,tf.float32),0)
-    boxes = tf.constant([[[0.05,0.05,0.9,0.7],[0.35,0.47,0.5,0.56]]])
-    # result = tf.image.draw_bounding_boxes(batched,boxes=boxes)
-    # plt.imshow(result[0].eval())
-    # print(result)
-    # 随机截取图片
-    begin,size,bbox_for_draw = tf.image.sample_distorted_bounding_box(tf.shape(img_data),
-                                                                      bounding_boxes=boxes,
-                                                                      min_object_covered=0.4)
-    batched = tf.expand_dims(tf.image.convert_image_dtype(img_data,tf.float32),0)
-    image_with_box = tf.image.draw_bounding_boxes(batched,bbox_for_draw)
-    distored_image = tf.slice(img_data,begin,size=size)
-    plt.imshow(distored_image.eval())
-    plt.show()
+#
+# with tf.Session() as sess:
+#     img_data = tf.image.decode_jpeg(image_data)
+#     img_data = tf.image.convert_image_dtype(img_data, dtype=tf.float32)
+#     # 将图片缩小一些，这样可视化能让标注框更加清楚
+#     img_data = tf.image.resize_images(img_data,[180,267],method=1)
+#     batched = tf.expand_dims(tf.image.convert_image_dtype(img_data,tf.float32),0)
+#     boxes = tf.constant([[[0.05,0.05,0.9,0.7],[0.35,0.47,0.5,0.56]]])
+#     # result = tf.image.draw_bounding_boxes(batched,boxes=boxes)
+#     # plt.imshow(result[0].eval())
+#     # print(result)
+#     # 随机截取图片
+#     begin,size,bbox_for_draw = tf.image.sample_distorted_bounding_box(tf.shape(img_data),
+#                                                                       bounding_boxes=boxes,
+#                                                                       min_object_covered=0.4)
+#     batched = tf.expand_dims(tf.image.convert_image_dtype(img_data,tf.float32),0)
+#     image_with_box = tf.image.draw_bounding_boxes(batched,bbox_for_draw)
+#     distored_image = tf.slice(img_data,begin,size=size)
+#     plt.imshow(distored_image.eval())
+#     plt.show()
--- TFdemo/7.2.2.py
+++ TFdemo/7.2.2.py
@@ -13,17 +13,19 @@ import matplotlib.pyplot as plt
 给定一张图片，随机调整图像的色彩。因为调整亮度、对比度、饱和度和色相的顺序会影响最后得到的结果，所以可以定义多种不同的顺序。
 具体使用哪一种顺序可以在训练数据预处理时随机的选择一种。这样可以进一步降低无关因素对模型的影响
 '''
-def distort_color(image,color_ordering = 0):
+
+
+def distort_color(image, color_ordering=0):
     if color_ordering == 0:
-        image = tf.image.random_brightness(image,max_delta=32./255)
-        image = tf.image.random_saturation(image,lower=0.5,upper=1.5)
-        image  = tf.image.random_hue(image,max_delta=0.2)
-        image = tf.image.random_contrast(image,lower=0.5,upper=1.5)
+        image = tf.image.random_brightness(image, max_delta=32. / 255)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
     elif color_ordering == 1:
         image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-        image = tf.image.random_brightness(image,max_delta=32./255)
+        image = tf.image.random_brightness(image, max_delta=32. / 255)
         image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-        image  = tf.image.random_hue(image,max_delta=0.2)
+        image = tf.image.random_hue(image, max_delta=0.2)
     elif color_ordering == 2:
         image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
         image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
@@ -32,38 +34,41 @@ def distort_color(image,color_ordering = 0):
         # 还可以添加其他的排列
     else:
         image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-        image = tf.image.random_brightness(image,max_delta=32./255)
+        image = tf.image.random_brightness(image, max_delta=32. / 255)
         image = tf.image.random_hue(image, max_delta=0.2)
-        image = tf.image.random_saturation(image,lower=0.5,upper=1.5)
-    return tf.clip_by_value(image,0.0,1.0)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+    return tf.clip_by_value(image, 0.0, 1.0)
 
 
-def preprocess_for_train(image,height,width,bbox):
+def preprocess_for_train(image, height, width, bbox):
     # 如果没有提供标准框，则认为整个图像就是需要关注的部分
     if bbox is None:
-        bbox = tf.constant([0.0,0.0,1.0,1.0],dtype=tf.float32,shape=[1,1,4])
+        bbox = tf.constant([0.0, 0.0, 1.0, 1.0], dtype=tf.float32, shape=[1, 1, 4])
     # 转换图像张量的类型：
     if image.dtype != tf.float32:
-        image = tf.image.convert_image_dtype(image,dtype=tf.float32)
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
     # 随机截取图像，减少需要关注的物体大小对图像识别算法的影响
-    bbox_begin,bbox_size,_ = tf.image.sample_distorted_bounding_box(tf.shape(image),bounding_boxes=bbox)
-    distorted_image = tf.slice(image,bbox_begin,bbox_size)
+    # 为图像生成一个随机distorted的边框,Generate a single randomly distorted bounding box for an image
+    bbox_begin, bbox_size, _ = tf.image.sample_distorted_bounding_box(tf.shape(image), bounding_boxes=bbox,min_object_covered=0.4)
+    distorted_image = tf.slice(image, bbox_begin, bbox_size)
+
     # 将随机截取的图像调整为神经网络输入层的大小。大小调整的算法是随机选择的。
-    distorted_image = tf.image.resize_images(distorted_image,[height,width],method=np.random.randint(4))
+    distorted_image = tf.image.resize_images(distorted_image, [height, width], method=np.random.randint(4))
     # 随机左右翻转图像
     distorted_image = tf.image.random_flip_left_right(distorted_image)
     # 使用一种随机的顺序处理图像的色彩
-    distorted_image = distort_color(distorted_image,np.random.randint(2))
-    return  distorted_image
+    distorted_image = distort_color(distorted_image, np.random.randint(2))
+    return distorted_image
 
-image_raw_data = tf.gfile.FastGFile("./data/5.jpg",'br').read()
+
+image_raw_data = tf.gfile.FastGFile("./data/dog1.jpg", 'br').read()
 
 with tf.Session() as sess:
     image_data = tf.image.decode_jpeg(image_raw_data)
-    bboxes = tf.constant([[[0.05,0.05,0.9,0.7],[0.35,0.47,0.5,0.56]]])
+    bboxes = tf.constant([[[0.05, 0.05, 0.9, 0.7], [0.35, 0.47, 0.5, 0.56]]])
 
     # 运行6次获得6中不同的图片，展示出来
     for i in range(6):
-        result = preprocess_for_train(image_data,299,299,bboxes)
+        result = preprocess_for_train(image_data, 299, 299, bboxes)
         plt.imshow(result.eval())
         plt.show()
